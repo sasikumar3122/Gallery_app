eb7c6704a6703b45b40a7a7a2efac3cb1bef56aa
sasi kumar <sasi.kumar@whitehatjr.com>
2022-05-12 11:17:13 +0530

    last commit

--- app/src/main/java/com/example/galleryapp/image.kt
+++ app/src/main/java/com/example/galleryapp/image.kt
@@ -6,12 +6,12 @@ class ImageData {
 
     var imagePath:String?=null
     var imageName:String?=null
-    var imgUri : Uri? = null
 
-    constructor(imagePath: String?, imageName: String?,imgUri: Uri?) {
+
+    constructor(imagePath: String?, imageName: String?) {
         this.imagePath = imagePath
         this.imageName = imageName
-        this.imgUri = imgUri
+
 
     }
     constructor(){}
